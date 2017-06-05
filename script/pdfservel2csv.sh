d6805b29ce3705d6fda4b1c353436eff2a5c2a1a
Marcelo M. A <ma.munoz.araya@gmail.com>
2017-06-04 22:24:48 -0400

    Mejora en pdfservel2csv.sh
    - Los script sed se han integrado dentro del script.
    - Se mejora el manejo de opciones
    - Se mejora documentación

--- script/pdfservel2csv.sh
+++ script/pdfservel2csv.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+#
+#  Este  script toma  los datos  del  padron electoral  del servel  en
+#  formato PDF y los transforma a formato de texto plano csv, el error
+#  de este escript  es del 0.58% para el padron  electoral del 2016 el
+#  cual tenia 13846209 registros
+#
 # USO:
-#  pdfservel2csv.sh  [-s] [-u] -o dest file1.pdf [file2.pdf ...]
+#  pdfservel2csv.sh  [-s]  -o dest file1.pdf [file2.pdf ...]
 #
 #  -s : No imprime header en el csv
 #  -o : Si dest es un directorio el resultado se pondra dentro caso
@@ -9,49 +14,79 @@
 #
 
 
-USAGE="pdfservel2csv.sh  [-s] [-u] -o dest file1.pdf [file2.pdf ...]"
 
 DEST=""
+USAGE="Uso: ./pdfservel2csv.sh  [-s] -o dest file1.pdf [file2.pdf ...]"
 HEADER="NOMBRE;RUT;SEXO;DOMICILIO ELECTORAL;CIRCUNSCRIPCIÓN;MESA"
 
 while getopts ":so:" opt; do
     case $opt in
-        s ) HEADER=""
+        s )
+            HEADER=""
             ;;
-        o ) DEST="$OPTARG"
-            [ -z "$DEST" ] && echo "dest debe ser un archivo o directorio" && exit 1
+        o )
+            DEST="$OPTARG"
+            if [[ -d ${DEST} ]]; then
+                ONEOUPUT="NO"
+            else
+                if touch $DEST 2> /dev/null ; then
+                    ONEOUPUT="YES"
+                else
+                    echo "Opcion -$opt requiere un existente directorio o una ruta valida de archivo"
+                    exit 1
+                fi
+            fi
             ;;
-        \? ) echo "$USAGE"
-             exit 1
-             ;;
+        \? )
+            echo "Opcion invalida: -$OPTARG"
+            echo "$USAGE"
+            exit 1
+            ;;
+        : )
+            echo "Opcion -$OPTARG require un argumento."
+            echo "$USAGE"
+            exit 1
+            ;;
     esac
 
 done
 
+
 shift $(($OPTIND - 1))
-
-echo $DEST
-echo $HEADER
-
-if [ -d $DEST ]; then
-    ONEOUPUT="NO"
-else
-    ONEOUPUT="YES"
+if [[ $# -eq 0 ]]; then
+    echo "Al parecer no ha especificado archivos para la conversión"
+    echo "$USAGE"
 fi
 
-echo $ONEOUPUT
+
+for file in "$@"; do
+    if [[ ! -e $file ]]; then
+        echo -e "Archivo: $file, \n No existe" >&2
+        echo "$USAGE"
+        exit 1
+    fi
+done
+
+# clean_sed : Quita headers de los archivos PDF
+# fix_sed   : pdftotext falla en formatear algunas lineas (filas aparecen en multiples lineas)
+# tocsv_sed : Heurística para detectar/marcar las columnas del archivo,  (el orden en que se aplica es necesario)
+
+clean_sed="/REPUBLICA/,/NOMBRE.*SEXO.*MESA$/d"
+fix_sed="N;N;N;N; s/\n  */ /; P; D"
+tocsv_sed="s/\s\{3,\}MUJ\s\{4,\}/\;MUJ\;\;/ ; s/\s\{3,\}VAR\s\{4,\}/\;VAR\;\;/ ; s/\s\{3\}MUJ\s\s\s/\;MUJ\;/ ; s/\s\{3\}VAR\s\s\s/\;VAR\;/ ; s/\s\{3,\}/\;/g"
+
 if [ "$ONEOUPUT" = "NO" ]; then
     for file in "$@"; do
         outfile="$DEST/$(basename "$file" .pdf).txt"
         echo "Procesando archivo: $file"
         [ ! -z "$HEADER" ] && echo "$HEADER" > "$outfile"
-        pdftotext -layout "$file" - | sed -f clean.sed | sed -f fix.sed | sed -f tocsv.sed >> "$outfile"
+        pdftotext -layout "$file" - | sed -e "$clean_sed" | sed -e "$fix_sed" | sed -e "$tocsv_sed" >> "$outfile"
     done
     echo "N"
 elif [ "$ONEOUPUT" = "YES" ]; then
     [ ! -z "$HEADER" ] && echo "$HEADER" > "$DEST"
     for file in "$@"; do
         echo "Procesando archivo: $file"
-        pdftotext -layout "$file" - | sed -f clean.sed | sed -f fix.sed | sed -f tocsv.sed >> "$DEST"
+        pdftotext -layout "$file" - | sed -e "$clean_sed" | sed -e "$fix_sed" | sed -e "$tocsv_sed" >> "$DEST"
     done
 fi
